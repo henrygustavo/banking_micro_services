28da4f6b1681143fbab29a484280df827dc601ac
henrygustavo <henrygustavof@gmail.com>
2018-08-21 00:22:10 -0500

    ini gateway

--- Customer/Customer.Api/Startup.cs
+++ Customer/Customer.Api/Startup.cs
@@ -10,7 +10,6 @@
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using System;
 
     public class Startup
     {
